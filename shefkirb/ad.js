8df645622bc206655bc10ea5175324cb21d9bbd1
shefwerld <rirurintoadgang@gmail.com>
2019-09-16 09:21:24 +0800

    fixed stuff

--- shefkirb/ad.js
+++ shefkirb/ad.js
@@ -1,11 +1,8 @@
-var y = Math.round(Math.random() * 4)
+var y = Math.round(Math.random() * 2);
   if (y == 1) {
-    document.getElementById('adlower').src="siteclockad.png"
+    document.getElementById('adlower').src="siteclockad.png";
+    document.getElementById('adlink').href="../shefmart/services/index.html";
   } else if (y == 2) {
-    document.getElementById('adlower').src="sitedad.png"
-    document.getElementById('adlink').href="../mgd/index.html"
-  } else if (y == 3)  {
-    document.getElementById('adlower').src="sitepigad.png"
-  } else  {
-    document.getElementById('adlower').src="siteblayydad.png"
+    document.getElementById('adlower').src="sitedad.png";
+    document.getElementById('adlink').href="../mgd/index.html";
   }
